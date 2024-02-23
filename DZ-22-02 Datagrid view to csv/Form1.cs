04e1fa83b86a84cb8f1313322283c3c056a2608d
ksenia 1234567890 <eurosetv3.04.05.14@gmail.com>
2024-02-23 13:36:22 +0300

    Добавьте файлы проекта.

---
+++ DZ-22-02 Datagrid view to csv/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DZ_22_02_Datagrid_view_to_csv
+{
+    public partial class ConvertCSV : Form
+    {
+        public ConvertCSV()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertCSV_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            // Список для того, чтобы значения не повторялись
+            List<char> lstSave = new List<char>();
+
+            char[] array = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    int tmp = random.Next(array.Length);
+                    if (Chech(lstSave, array[tmp]))
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = array[tmp];
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+            }
+
+            openFileDialog1.FileName = @"data\Text2.txt";
+            openFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|Excel-файлы (*.csv)|*.csv|All files (*.*)|*.*";
+            saveFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|Excel-файлы (*.csv)|*.csv|All files (*.*)|*.*";
+        }
+            private bool Chech(List<char> lst, char ch)
+            {
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    if (lst[i] == ch)
+                    {
+                        return false;
+                    }
+                }
+                // Данного значения не было в DataGridView
+                lst.Add(ch);
+                return true;
+            }
+        
+
+        private void export_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Excel files (*.csv)|*.csv|Text files (*.txt)|*.txt";
+
+            if(dlg.ShowDialog()==DialogResult.OK)
+            {
+                Stream myStream;
+                myStream = dlg.OpenFile();
+                StreamWriter sw = new StreamWriter(myStream, System.Text.Encoding.GetEncoding(-0));
+                string columnTitle = "";
+                try
+                {
+                    for (int i=0;i< dataGridView1.ColumnCount;i++)
+                    {
+                        if(i>0)
+                        {
+                            columnTitle += " ";
+                        }
+                        columnTitle += dataGridView1.Columns[i].HeaderText;
+                    }
+                    sw.WriteLine(columnTitle);
+
+                    // Написать содержимое столбца
+
+                    for(int j = 0; j<dataGridView1.Rows.Count;j++)
+                    {
+                        string columnValue = "";
+                        for(int k=0;k<dataGridView1.Columns.Count; k++)
+                        {
+                            if(k>0)
+                            {
+                                columnValue += " ";
+                            }
+                            if (dataGridView1.Rows[j].Cells[k].Value == null)
+                                columnValue += "";
+                            else if (dataGridView1.Rows[j].Cells[k].Value.ToString().Contains(" "))
+                            {
+                                columnValue += "\"" + dataGridView1.Rows[j].Cells[k].Value.ToString().Trim() + "\"";
+                            }
+                            else
+                            {
+                                columnValue += dataGridView1.Rows[j].Cells[k].Value.ToString().Trim() + "\t";
+                            }
+                        }
+                        sw.WriteLine(columnValue);
+
+                    }
+                    sw.Close();
+                    myStream.Close();
+                    MessageBox.Show("Данные экспортированы");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Форма экспорта не удалась");
+                }
+                finally
+                {
+                    sw.Close();
+                    myStream.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Отмените операцию экспорта");
+            }
+
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ DZ-22-02 Datagrid view to csv/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("DZ-22-02 Datagrid view to csv")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DZ-22-02 Datagrid view to csv")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0c5fc018-10b4-4f32-9efe-eb4aeb6879e7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
